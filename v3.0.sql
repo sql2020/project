c55d6079e9086bb6568f1d5781dc7fe116422614
Longday0923 <65357559+Longday0923@users.noreply.github.com>
2020-05-24 10:11:11 +0800

    Update v3.0.sql

--- v3.0.sql
+++ v3.0.sql
@@ -18,7 +18,8 @@ create table zones(
 create table admins(
 	aid varchar(9) primary key,
 	Aname varchar(20) not null,
-	Aemail varchar(30) not null
+	Aemail varchar(30) not null,
+	zone_id varchar(2) references zones on delete set null on update cascade
 );
 
 
@@ -26,8 +27,10 @@ create table admins(
 create table works(
 	AV varchar(9) primary key,
 	diy bool default true,
-	Works_create_date timestamp not null,
-	VIP_limit bool default false
+	Works_create_date date not null,
+	VIP_limit bool default false,
+	zone_id varchar(2) references zones on delete set null on update cascade,
+	aid varchar(9) references admins on delete no action
 );
 
 -- vip
@@ -40,7 +43,7 @@ create table vip(
 -- posts
 create table posts(
 	post_id varchar(9) primary key,
-	post_date timestamp not null,
+	post_date date not null,
 	post_type bool default true,
 	post_content varchar(30) not null
 );
@@ -50,14 +53,14 @@ create table posts(
 create table uploads(
 	av varchar(9) references works on delete cascade,
 	uid varchar(9) references users on delete cascade,
-	upload_date timestamp not null,
+	upload_date date not null,
 	primary key (av , uid)
 );
 -- collection 
 create table collection(
 	av varchar(9) references works on delete cascade,
 	uid varchar(9) references users on delete cascade,
-	collection_date timestamp not null,
+	collection_date date not null,
 	primary key (av , uid)
 );
 
@@ -65,7 +68,7 @@ create table collection(
 create table subscribe(
 	av varchar(9) references works on delete cascade,
 	uid varchar(9) references users on delete cascade,
-	subscribe_date timestamp not null,
+	subscribe_date date not null,
 	primary key (av , uid)
 );
 
@@ -73,7 +76,7 @@ create table subscribe(
 create table play(
 	av varchar(9) references works on delete cascade,
 	uid varchar(9) references users on delete no action,
-	play_date timestamp not null,
+	play_date date not null,
 	play_state numeric(2,2) default 0,
 	primary key(av, uid,play_date)
 );
@@ -82,7 +85,7 @@ create table play(
 create table likes(
 	av varchar(9) references works on delete cascade,
 	uid varchar(9) references users on delete no action,
-	like_date timestamp not null,
+	like_date date not null,
 	primary key (av , uid)
 );
 
@@ -90,7 +93,7 @@ create table likes(
 create table coins(
 	av varchar(9) references works on delete no action,
 	uid varchar(9) references users on delete no action,
-	coins_date timestamp not null,
+	coins_date date not null,
 	coins_num numeric(1,0) default 1,
 	primary key (av , uid)
 );
@@ -99,7 +102,7 @@ create table coins(
 create table follow(
 	UID1 varchar(9) references users on delete cascade,
 	UID2 varchar(9) references users on delete cascade,
-	follow_date timestamp not null,
+	follow_date date not null,
 	primary key (UID1,UID2)
 );
 
@@ -107,7 +110,7 @@ create table follow(
 create table Danmaku(
 	av varchar(9) references works on delete cascade,
 	uid varchar(9) references users on delete no action,
-	Dan_create_date timestamp not null,
+	Dan_create_date date not null,
 	time_stamp time not null,
 	Dan_level int default'1',check (Dan_level in ('1','2','3')),
 	dan_content  varchar(50),
@@ -121,7 +124,7 @@ create table posts_comment(
 	uid varchar(9) references users on delete no action,
 	post_id varchar(9) references posts(post_id) on delete cascade,
 	post_comment_content varchar(40) not null,
-	Post_comment_date timestamp not null,
+	Post_comment_date date not null,
 	primary key (post_id,Post_comment_date)
 );
 
@@ -131,7 +134,7 @@ create table works_comment(
 	uid varchar(9) references users on delete no action,
 	av varchar(9) references works on delete cascade,
 	works_comment_content varchar(40) not null,
-	works_comment_date timestamp not null,
+	works_comment_date date not null,
 	primary key (av,works_comment_date)
 );
 
@@ -140,8 +143,8 @@ create table works_comment(
 create table shares(
 	uid varchar(9) references users on delete no action,
 	av varchar(9) references works on delete cascade,
-	share_date timestamp not null,
-	share_destin varchar(20) not null,
+	share_date date not null,
+	share_destin varchar(20) default 'bilibili', check (share_destin in ('bilibili','weixin','qq','weibo','douban','unknown')),
 	primary key(av,share_date,uid)
 );
 
@@ -150,15 +153,8 @@ create table shares(
 create table posting(
 	post_id varchar(9) references posts(post_id) on delete cascade,
 	uid varchar(9) references users on delete no action,
-	posting_date timestamp not null,
+	posting_date date not null,
 	primary key(post_id)
-);
-
---works_zones
-create table works_zones(
-	av varchar(9) references works on delete cascade,
-	zone_id varchar(2) references zones on delete set null on update cascade,
-	primary key (av)
 );
 
 --become_vip
@@ -167,18 +163,4 @@ create table become_vip(
 	vip_signup_date timestamp not null ,
 	vip_type varchar(2) references vip on delete set null on update cascade,
 	primary key(uid,vip_signup_date)
-);
-
---admin_zones
-create table admin_zones(
-	aid varchar(9) references admins on delete cascade,
-	zone_id varchar(2) references zones on delete set null on update cascade,
-	primary key(aid)
-);
-
--- examines
-create table examines(
-	aid varchar(9) references admins on delete no action,
-	av varchar(9) references works on delete no action,
-	primary key (av)
 );
