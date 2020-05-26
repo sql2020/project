904b3efd7c054ec22805e4ed9c5cd2413d4a3d1e
Longday0923 <65357559+Longday0923@users.noreply.github.com>
2020-05-26 10:36:45 +0800

    Add files via upload

---
+++ select部分.sql
@@ -0,0 +1,16 @@
+--检索作品
+--按分区
+select av from works
+where zone_id = '01';
+--up主
+select av from works
+where uid = 'u10001';
+--可以加在后面的附加条件
+--按创建时间（最新）
+order by works_create_date desc;
+--按播放量多优先
+select av 
+from works, play_num
+where works.uid = play_num.uid
+--这里还可以再添加其他约束
+order by play_num.sum;
